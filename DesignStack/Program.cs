244879d96ccef904f05deaac8f96dd24f7f14409
Sinbad1 <fatyr13@gmail.com>
2020-12-14 14:23:29 +0100

    Initial commit

---
+++ DesignStack/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DesignStack
+{
+    class Program
+    {
+    static void Main()
+        {
+            string welcome = "Welcome! This is a LISP example project. Add, remove and clear from a stack. The following commands:";
+            string options = "--------Options--------\n       Add: 'a'\n       Remove: 'r'\n       Clear: 'c'\n       Stack List: 's'\n       Quit: 'q'\n--------Options--------";
+            var stack = new Stack();
+            int stackCount = 0;
+
+            ConsoleSpace.Spacing(welcome, options);
+
+            while (true)
+            {
+                try
+                {
+                    switch (Console.ReadLine().ToLower())
+                    {
+                        case "a":
+                            Console.Clear();
+                            ConsoleSpace.Spacing(welcome, options);
+                            stack.Push(stackCount++);
+                            Console.WriteLine($"Current stack count is: {stack.StackCount()}");
+                            break;
+
+                        case "r":
+                            Console.Clear();
+                            ConsoleSpace.Spacing(welcome, options);
+                            stack.Pop();
+                            Console.WriteLine($"Current stack count is: {stack.StackCount()}");
+                            break;
+
+                        case "c":
+                            Console.Clear();
+                            ConsoleSpace.Spacing(welcome, options);
+                            stack.Clear();
+                            Console.WriteLine($"Current stack count is: {stack.StackCount()}");
+                            break;
+
+                        case "s":
+                            Console.Clear();
+                            ConsoleSpace.Spacing(welcome, options);
+                            Console.WriteLine($"Current stack:");
+                            stack.StackDisplay();
+                            break;
+
+                        case "q":
+                        return;
+
+                        default:
+                                Console.Clear();
+                                ConsoleSpace.Spacing(welcome, options);
+                                Console.WriteLine("Please input a valid option.");
+                            break;
+                    }
+                }
+                catch (InvalidOperationException)
+                {
+                    Console.Clear();
+                    ConsoleSpace.Spacing(welcome, options);
+                    Console.WriteLine("There is nothing in the stack.");
+                }
+            }
+        }
+    }
+}
---
+++ DesignStack/Stack.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignStack
+{
+    internal class Stack
+    {
+        private List<Object> stack = new List<Object>();
+
+        public void Clear()
+        {
+            if (stack.Count != 0)
+            {
+                stack.Clear();
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public object Pop()
+        {
+
+            if (stack.Count != 0)
+            {
+                stack.RemoveAt(stack.Count - 1);
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+            return stack.Count + 1;
+        }
+        public void Push(object obj)
+        {
+            if (obj is object)
+            {
+                stack.Add(obj);
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public object StackCount()
+        {
+            return stack.Count;
+        }
+        public object StackDisplay()
+        {
+
+            foreach (Object obj in stack)
+            {
+                Console.WriteLine(stack.IndexOf(obj) + 1);
+            }
+            return stack;
+        }
+    }
+}
